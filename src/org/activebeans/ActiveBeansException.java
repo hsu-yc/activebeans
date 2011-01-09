ff972babd610ce08a41aac2925097a678c001094
hsu.yaochung <hsu.yaochung@cf13b5f8-ad1c-ab8c-9581-fc86a4c079d6>
2011-01-09 16:53:02 +0000

    added all constructors from RuntimeException

--- src/org/activebeans/ActiveBeansException.java
+++ src/org/activebeans/ActiveBeansException.java
@@ -3,8 +3,20 @@ package org.activebeans;
 @SuppressWarnings("serial")
 public class ActiveBeansException extends RuntimeException {
 
-	public ActiveBeansException(Throwable t) {
-		super(t);
+	public ActiveBeansException() {
+		super();
+	}
+
+	public ActiveBeansException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ActiveBeansException(String message) {
+		super(message);
+	}
+
+	public ActiveBeansException(Throwable cause) {
+		super(cause);
 	}
 
 }
