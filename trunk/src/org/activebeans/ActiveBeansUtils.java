950fbd656731d3a186835dc172050d4c256e8c38
hsu.yaochung <hsu.yaochung@cf13b5f8-ad1c-ab8c-9581-fc86a4c079d6>
2011-01-16 15:46:27 +0000

    converts camel case to underscore

--- trunk/src/org/activebeans/ActiveBeansUtils.java
+++ trunk/src/org/activebeans/ActiveBeansUtils.java
@@ -1,6 +1,8 @@
 package org.activebeans;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public final class ActiveBeansUtils {
@@ -15,6 +17,81 @@ public final class ActiveBeansUtils {
 			map.put(i.getName(), i);
 		}
 		return map;
+	}
+
+	public static String camelCaseToUnderscore(String input) {
+		List<String> toks = new ArrayList<String>();
+		for (String tok : splitByCharacterType(input, true)) {
+			toks.add(tok.toLowerCase());
+		}
+		return join(toks.toArray(), "_", 0, toks.size());
+	}
+
+	private static String[] splitByCharacterType(String str, boolean camelCase) {
+		if (str == null) {
+			return null;
+		}
+		if (str.length() == 0) {
+			return new String[0];
+		}
+		char[] c = str.toCharArray();
+		List<String> list = new ArrayList<String>();
+		int tokenStart = 0;
+		int currentType = Character.getType(c[tokenStart]);
+		for (int pos = tokenStart + 1; pos < c.length; pos++) {
+			int type = Character.getType(c[pos]);
+			if (type == currentType) {
+				continue;
+			}
+			if (camelCase && type == Character.LOWERCASE_LETTER
+					&& currentType == Character.UPPERCASE_LETTER) {
+				int newTokenStart = pos - 1;
+				if (newTokenStart != tokenStart) {
+					list.add(new String(c, tokenStart, newTokenStart
+							- tokenStart));
+					tokenStart = newTokenStart;
+				}
+			} else {
+				list.add(new String(c, tokenStart, pos - tokenStart));
+				tokenStart = pos;
+			}
+			currentType = type;
+		}
+		list.add(new String(c, tokenStart, c.length - tokenStart));
+		return list.toArray(new String[list.size()]);
+	}
+
+	private static String join(Object[] array, String separator,
+			int startIndex, int endIndex) {
+		if (array == null) {
+			return null;
+		}
+		if (separator == null) {
+			separator = "";
+		}
+
+		// endIndex - startIndex > 0: Len = NofStrings *(len(firstString) +
+		// len(separator))
+		// (Assuming that all Strings are roughly equally long)
+		int bufSize = (endIndex - startIndex);
+		if (bufSize <= 0) {
+			return "";
+		}
+
+		bufSize *= ((array[startIndex] == null ? 16 : array[startIndex]
+				.toString().length()) + separator.length());
+
+		StringBuilder buf = new StringBuilder(bufSize);
+
+		for (int i = startIndex; i < endIndex; i++) {
+			if (i > startIndex) {
+				buf.append(separator);
+			}
+			if (array[i] != null) {
+				buf.append(array[i]);
+			}
+		}
+		return buf.toString();
 	}
 
 }
