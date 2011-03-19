6e7c9b2a6ce375388e053104bb1e373fd7d2cdc5
hsu.yaochung@gmail.com <hsu.yaochung@gmail.com@cf13b5f8-ad1c-ab8c-9581-fc86a4c079d6>
2011-03-19 05:27:11 +0000

    refactor active beans method filter

--- trunk/src/org/activebeans/ClassMethodFilter.java
+++ trunk/src/org/activebeans/ClassMethodFilter.java
@@ -1,6 +1,7 @@
 package org.activebeans;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,10 +9,12 @@ import javassist.util.proxy.MethodFilter;
 
 public class ClassMethodFilter implements MethodFilter {
 	
-	private List<Method> methods;
-
-	public ClassMethodFilter(Class<?> clazz){
-		methods = Arrays.asList(clazz.getMethods());
+	private List<Method> methods = new ArrayList<Method>();
+	
+	public ClassMethodFilter(Class<?>... classes){
+		for (Class<?> c : classes) {
+			methods.addAll(Arrays.asList(c.getMethods()));
+		}
 	}
 	
 	@Override
