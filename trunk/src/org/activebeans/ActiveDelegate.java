0b32b78767ff20630a6d8450601539cddb819a97
hsu.yaochung@gmail.com <hsu.yaochung@gmail.com@cf13b5f8-ad1c-ab8c-9581-fc86a4c079d6>
2011-03-24 16:50:38 +0000

    implement attr() of Model

--- trunk/src/org/activebeans/ActiveDelegate.java
+++ trunk/src/org/activebeans/ActiveDelegate.java
@@ -1,34 +1,45 @@
 package org.activebeans;
 
 import java.lang.reflect.Method;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import javassist.util.proxy.MethodFilter;
 import javassist.util.proxy.MethodHandler;
 
 public class ActiveDelegate implements MethodFilter, MethodHandler {
+	
+	private ClassMethodFilter filter;
 
-	private MapDelegate delegate;
+	private AttributeMethodHandler attrHandler;
 
+	private ClassMethodFilter attrFilter;
+
+	private ClassMethodFilter modelFilter;
+
+	private ModelMethodHandler modelHandler;
+	
 	public ActiveDelegate(Class<? extends Model<?, ?, ?, ?>> activeClass) {
 		ActiveIntrospector intro = new ActiveIntrospector(activeClass);
-		Map<MethodFilter, MethodHandler> map = new LinkedHashMap<MethodFilter, MethodHandler>();
-		AttributeMethodHandler attrs = new AttributeMethodHandler(activeClass);
-		map.put(new ClassMethodFilter(intro.attributesInterface()), attrs);
-		map.put(new ClassMethodFilter(Model.class), Delegate.of(new ModelDelegate(attrs)));
-		delegate = new MapDelegate(map);
+		Class<?> attrInterf = intro.attributesInterface();
+		@SuppressWarnings("rawtypes")
+		Class<Model> modelClass = Model.class;
+		filter = new ClassMethodFilter(attrInterf, modelClass);
+		attrFilter = new ClassMethodFilter(attrInterf);
+		attrHandler = new AttributeMethodHandler(activeClass);
+		modelFilter = new ClassMethodFilter(modelClass);
+		modelHandler = new ModelMethodHandler(attrHandler);
 	}
-
+	
 	@Override
 	public Object invoke(Object self, Method method, Method proceed,
 			Object[] args) throws Throwable {
 		Object rtn = null;
 		if (proceed != null) {
 			rtn = proceed.invoke(self, args);
-		} else if(delegate.isHandled(method)){
-			rtn = delegate.invoke(self, method, proceed, args);
-		} else {
+		} else if(attrFilter.isHandled(method)){
+			rtn = attrHandler.invoke(self, method, proceed, args);
+		} else if(modelFilter.isHandled(method)){
+			rtn = modelHandler.invoke(self, method, proceed, args);
+		}else {
 			rtn = ActiveBeansUtils.defaultValue(method.getReturnType());
 		}
 		return rtn;
@@ -36,7 +47,7 @@ public class ActiveDelegate implements MethodFilter, MethodHandler {
 	
 	@Override
 	public boolean isHandled(Method m) {
-		return delegate.isHandled(m);
+		return filter.isHandled(m);
 	}
 
 }
---
+++ trunk/src/org/activebeans/ModelMethodHandler.java
@@ -0,0 +1,57 @@
+package org.activebeans;
+
+import java.lang.reflect.Method;
+import java.util.Map.Entry;
+
+import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.ProxyObject;
+
+@SuppressWarnings("rawtypes")
+public class ModelMethodHandler implements Model, MethodHandler {
+	
+	private Object self;
+	private AttributeMethodHandler attrHandler;
+	private Delegate delegate;
+	
+	public ModelMethodHandler(AttributeMethodHandler attrHandler){
+		this.attrHandler = attrHandler;
+		delegate = new Delegate(this);
+	}
+	
+	@Override
+	public boolean save() {
+		return false;
+	}
+
+	@Override
+	public boolean update() {
+		return false;
+	}
+
+	@Override
+	public boolean update(Object attrs) {
+		return false;
+	}
+
+	@Override
+	public Model attrs(Object attrs) {
+		OptionsMethodHandler options = (OptionsMethodHandler) ((ProxyObject)(attrs)).getHandler();
+		for (Entry<Property, Object> e : options.properties().entrySet()) {
+			attrHandler.set(e.getKey(), e.getValue());
+		}
+		return (Model) self;
+	}
+
+	@Override
+	public boolean destroy() {
+		return false;
+	}
+	
+	@Override
+	public Object invoke(Object self, Method method, Method proceed,
+			Object[] args) throws Throwable {
+		this.self = self;
+		return delegate.invoke(self, method, proceed, args);
+	}
+
+}
